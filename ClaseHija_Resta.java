d916e37e6f28753d19c847e8a676d330a2078a58
Jairo Arboleda <jairo.arboledal6@gmail.com>
2023-06-29 23:23:52 -0500

    segundo commit

---
+++ ClaseHija_Resta.java
@@ -0,0 +1,8 @@
+package poo.Herencia1.Operaciones;
+
+public class ClaseHija_Resta extends ClasePadre {
+    public void restar() {
+        System.out.println("Valores a restar: ");
+        resultado=valor1-valor2;
+    }
+}
---
+++ ClaseHija_suma.java
@@ -0,0 +1,8 @@
+package poo.Herencia1.Operaciones;
+
+public class ClaseHija_suma extends ClasePadre {
+    public void sumar() {
+        resultado= valor1+valor2;
+    }
+}
+
